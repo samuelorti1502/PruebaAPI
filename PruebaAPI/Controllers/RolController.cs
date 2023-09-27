ab9c025c481521f43494046c949c0ee77a53a574
Samuel Ortiz <samuel.om@alliedglobal.com>
2023-09-26 22:04:00 -0600

    Se agregan metodos de insertar y modificar.

--- PruebaAPI/Controllers/RolController.cs
+++ PruebaAPI/Controllers/RolController.cs
@@ -11,14 +11,69 @@ namespace PruebaAPI.Controllers
     [Route("api/rol")]
     public class RolController : Controller
     {
+        private readonly Metodos_Rol _metodosRol;
+
+
         [HttpGet]
         [Route("listar")]
         public async Task<ActionResult<List<Rol>>> Get()
         {
-            var datos = new Metodos_Rol();
-            var lista = await datos.MostrarRoles();
-            return lista;
+            try
+            {
+                var datos = new Metodos_Rol();
+                var lista = await datos.MostrarRoles();
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error interno del servidor: " + ex.Message);
+            }
+            
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Rol>>> MostrarRol_id(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    return BadRequest("El ID de rol debe ser un número entero positivo.");
+                }
+
+                // Obtener el rol
+                var datos = new Metodos_Rol();
+                var roles = await datos.MostrarRol_id(id);
+
+                if (roles == null)
+                {
+                    return NotFound("No se encontró un rol con el ID proporcionado.");
+                }
+
+                // Devolver la respuesta
+                return Ok(roles);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error interno del servidor: " + ex.Message);
+            }
+            
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Rol parametros)
+        {
+            var funcion = new Metodos_Rol();
+            await funcion.InsertarRol(parametros);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Rol parametros)
+        {
+            var funcion = new Metodos_Rol();
+            parametros.id = id;
+            await funcion.ModificarRol(parametros);
+            return new OkResult();
+        }
     }
 }
--- PruebaAPI/Metodos/Rol.cs
+++ PruebaAPI/Metodos/Rol.cs
@@ -1,71 +1,84 @@
-﻿using PruebaAPI.Conexion;
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaAPI.Conexion;
 using PruebaAPI.Models;
 using System.Data.SqlClient;
 
 public class Metodos_Rol
+{
+    private ConexionDB conexion = new ConexionDB();
+
+    private void AgregarParametro(SqlCommand cmd, string nombre, object valor)
     {
-        private ConexionDB conexion = new ConexionDB();
-
-        private async Task<List<Rol>> EjecutarSP(int accion, int id, string rol, string descripcion, int usuarioCreacion)
+        if (valor == null)
         {
-            var lista = new List<Rol>();
+            cmd.Parameters.AddWithValue(nombre, DBNull.Value);
+        }
+        else
+        {
+            cmd.Parameters.AddWithValue(nombre, valor);
+        }
+    }
 
-            using (var sql = new SqlConnection(conexion.GetConexion()))
-            using (var cmd = new SqlCommand("sp_roles_crud", sql))
+    private async Task<List<Rol>> EjecutarSP(int accion, int? id, string rol, string descripcion, int usuarioCreacion)
+    {
+        var lista = new List<Rol>();
+
+        using (var sql = new SqlConnection(conexion.GetConexion()))
+        using (var cmd = new SqlCommand("sp_roles_crud", sql))
+        {
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            AgregarParametro(cmd, "@accion", accion);
+            AgregarParametro(cmd, "@id", id);
+            AgregarParametro(cmd, "@rol", rol);
+            AgregarParametro(cmd, "@descripcion", descripcion);
+            AgregarParametro(cmd, "@usuario_creacion", usuarioCreacion);
+
+            await sql.OpenAsync();
+
+            using (var leer = await cmd.ExecuteReaderAsync())
             {
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@accion", accion);
-                cmd.Parameters.AddWithValue("@id", id);
-                cmd.Parameters.AddWithValue("@rol", rol);
-                cmd.Parameters.AddWithValue("@descripcion", descripcion);
-                cmd.Parameters.AddWithValue("@usuario_creacion", usuarioCreacion);
-
-                await sql.OpenAsync();
-
-                using (var leer = await cmd.ExecuteReaderAsync())
+                while (await leer.ReadAsync())
                 {
-                    while (await leer.ReadAsync())
+                    var M_Rol = new Rol
                     {
-                        var M_Rol = new Rol
-                        {
-                            opcion = accion,
-                            id = (int)leer["id"],
-                            rol = (string)leer["rol"],
-                            descripcion = (string)leer["descripcion"],
-                            usuario_creacion = (int)leer["usuario_creacion"]
-                        };
+                        opcion = accion,
+                        id = (int)leer["id"],
+                        rol = (string)leer["rol"],
+                        descripcion = (string)leer["descripcion"],
+                        //estatus = (string)leer["descripcion"],
+                        usuario_creacion = (int)leer["usuario_creacion"]
+                    };
 
-                        lista.Add(M_Rol);
-                    }
+                    lista.Add(M_Rol);
                 }
             }
-
-            return lista;
         }
 
-        public async Task<List<Rol>> MostrarRoles()
-        {
-            return await EjecutarSP(4, 0, "", "", 3);
-        }
-
-        /*public async Task<List<Rol>> MostrarRol_id(Rol parametros)
-        {
-            return await EjecutarSP(parametros.opcion, parametros.id, "", "", parametros.usuario_creacion);
-        }
-
-        /*public async Task InsertarRol(Rol parametros)
-        {
-            await EjecutarSP(parametros.opcion, 0, parametros.rol, parametros.descripcion, parametros.usuario_creacion);
-        }
-
-        public async Task ModificarRol(Rol parametros)
-        {
-            await EjecutarSP(parametros.opcion, parametros.id, parametros.rol, parametros.descripcion, parametros.usuario_creacion);
-        }
-
-        public async Task EliminarRol(Rol parametros)
-        {
-            await EjecutarSP(parametros.opcion, parametros.id, parametros.rol, parametros.descripcion, parametros.usuario_creacion);
-        }*/
+        return lista;
     }
+
+    public async Task<List<Rol>> MostrarRoles()
+    {
+        return await EjecutarSP(4, 0, "", "", 3);
+    }
+    public async Task<List<Rol>> MostrarRol_id(int id)
+    {
+        
+        return await EjecutarSP(5, id, "", "", 3);
+    }
+    public async Task InsertarRol(Rol parametros)
+    {
+        await EjecutarSP(1, 0, parametros.rol, parametros.descripcion, parametros.usuario_creacion);
+    }
+    public async Task ModificarRol(Rol parametros)
+    {
+        await EjecutarSP(2, parametros.id, parametros.rol, parametros.descripcion, parametros.usuario_creacion);
+    }
+
+    /*public async Task EliminarRol(Rol parametros)
+    {
+        await EjecutarSP(parametros.opcion, parametros.id, parametros.rol, parametros.descripcion, parametros.usuario_creacion);
+    }*/
+}
 
