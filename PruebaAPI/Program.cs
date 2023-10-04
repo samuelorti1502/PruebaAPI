4d96bb5128b5f71ac8693854fdf06ee2ddc515f4
samuelorti1502 <sortizm5@miumg.edu.gt>
2023-10-04 13:49:42 -0600

    Se agrega configuracion de cors

--- PruebaAPI/Program.cs
+++ PruebaAPI/Program.cs
@@ -7,7 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(policyBuilder =>
+    policyBuilder.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyHeader())
+);
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
